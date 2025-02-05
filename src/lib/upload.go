9048db978aeb3c23f3d2810a93ba0f6b5f17030e
Mohammad Hosein Balkhani <mohammadhb25@gmail.com>
2025-02-06 00:18:46 +0330

    Merge Main to Production (#50)
    * fix(refactore_package): use publice package of database
    
    Signed-off-by: jeyem <me@e-mahmoudi.me>
    
    * rerunner
    
    * feat: add gcs upload
    
    * feat: adding share folder to shin
    
    * fix: fixed uploader using wrong filename to upload
    
    ---------
    
    Signed-off-by: jeyem <me@e-mahmoudi.me>
    Co-authored-by: jeyem <me@e-mahmoudi.me>

--- src/lib/upload.go
+++ src/lib/upload.go
@@ -15,6 +15,9 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"google.golang.org/api/option"
+
+	"cloud.google.com/go/storage"
 )
 
 type S3ConfigType struct {
@@ -27,6 +30,17 @@ type S3ConfigType struct {
 
 var S3Client *s3.Client
 var S3Config S3ConfigType
+
+type GCSConfigType struct {
+	Bucket          string
+	CDNUrl          string
+	CredentialsPath string
+}
+
+var GCSContext context.Context
+var GCSClient *storage.Client
+var GCSConfig GCSConfigType
+
 var mimeTypeToExt = map[string]string{
 	"image/png":          "png",
 	"image/jpeg":         "jpg",
@@ -45,7 +59,7 @@ func hashFile(file io.Reader) (string, error) {
 	return hex.EncodeToString(hash.Sum(nil)), nil
 }
 
-func Upload(file multipart.File, fileName string) (string, string) {
+func UploadS3(file multipart.File, fileName string) (string, string) {
 
 	fileContent, _ := io.ReadAll(file)
 
@@ -73,6 +87,40 @@ func Upload(file multipart.File, fileName string) (string, string) {
 	return fmt.Sprintf("%s/%s", S3Config.CDNUrl, filename), fileName
 }
 
+func UploadGCS(file multipart.File, fileName string) (string, string) {
+
+	// Read file content into memory
+	fileContent, _ := io.ReadAll(file)
+	hash, _ := hashFile(bytes.NewReader(fileContent))
+
+	// Read the first 512 bytes of the file & Detect the file's content type
+	buf := fileContent[:512]
+	mimeType := http.DetectContentType(buf)
+	ext := mimeTypeToExt[mimeType]
+	if mimeType == "application/octet-stream" {
+		splitName := strings.Split(fileName, ".")
+		ext = splitName[len(splitName)-1]
+	}
+
+	filename := fmt.Sprintf("%s.%s", hash, ext)
+
+	bucket := GCSClient.Bucket(GCSConfig.Bucket)
+	wc := bucket.Object(filename).NewWriter(GCSContext)
+	wc.ContentType = mimeType
+
+	wc.Write(fileContent)
+	wc.Close()
+	return fmt.Sprintf("%s/%s", GCSConfig.CDNUrl, filename), fileName
+
+}
+
+func Upload(file multipart.File, fileName string) (string, string) {
+	if S3Client != nil {
+		return UploadS3(file, fileName)
+	}
+	return UploadGCS(file, fileName)
+}
+
 // Initializing
 func InitS3Lib(configs S3ConfigType) {
 	S3Config = configs
@@ -85,4 +133,11 @@ func InitS3Lib(configs S3ConfigType) {
 		HTTPClient:       &http.Client{Timeout: 30 * time.Second},
 		ClientLogMode:    aws.LogRequestWithBody | aws.LogResponseWithBody,
 	})
+}
+
+func InitGCSLib(configs GCSConfigType) {
+	GCSConfig = configs
+	GCSContext = context.Background()
+	client, _ := storage.NewClient(GCSContext, option.WithCredentialsFile(GCSConfig.CredentialsPath))
+	GCSClient = client
 }
