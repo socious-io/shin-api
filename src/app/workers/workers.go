5e0f47259496527153893af1117217befe480a90
Mohammad Hosein Balkhani <mohammadhb25@gmail.com>
2025-09-25 18:01:35 +0330

    fix: align consumers format fix: centralized workflows

--- src/app/workers/workers.go
+++ src/app/workers/workers.go
@@ -4,30 +4,30 @@ import (
 	"github.com/socious-io/gomq"
 )
 
-var consumers = []gomq.AddConsumerParams{
-	{
-		Channel:       "sociousid/event:user.delete",
-		Consumer:      gomq.NewConsumer(DeleteUser),
-		IsCategorized: false,
-	},
-	{
-		Channel:       "sociousid/event:identities.sync",
-		Consumer:      gomq.NewConsumer(SyncIdentities),
-		IsCategorized: false,
-	},
-	{
-		Channel:       "import",
-		Consumer:      gomq.NewConsumer(ImportWorker),
-		IsCategorized: true,
-	},
-	{
-		Channel:       "operation",
-		Consumer:      gomq.NewConsumer(OperationWorker),
-		IsCategorized: true,
-	},
-}
-
 func RegisterConsumers() {
+	var consumers = []gomq.AddConsumerParams{
+		{
+			Channel:       "sociousid/event:user.delete",
+			Consumer:      gomq.NewConsumer(DeleteUser),
+			IsCategorized: false,
+		},
+		{
+			Channel:       "sociousid/event:identities.sync",
+			Consumer:      gomq.NewConsumer(SyncIdentities),
+			IsCategorized: false,
+		},
+		{
+			Channel:       "import",
+			Consumer:      gomq.NewConsumer(ImportWorker),
+			IsCategorized: true,
+		},
+		{
+			Channel:       "operation",
+			Consumer:      gomq.NewConsumer(OperationWorker),
+			IsCategorized: true,
+		},
+	}
+
 	for _, consumer := range consumers {
 		gomq.AddConsumer(consumer)
 	}
